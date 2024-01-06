4755d46b258ffafb068be4e8aa50bc2a76f1ae3b
Logiase Song <logiase.song@gmail.com>
2024-01-06 22:34:41 +0800

    strip result blank line

--- nxp-sdk.cmake
+++ nxp-sdk.cmake
@@ -1,14 +1,17 @@
 find_package(Python3 REQUIRED COMPONENTS Interpreter)
 
+set(_NXP_SDK_PYTHON_SCRIPT_PATH ${CMAKE_CURRENT_LIST_DIR}/nxp_sdk_parser.py)
+
 function(nxp_sdk)
     set(ONE_VALUE_ARGS SDK_ROOT VARIABLE)
     set(MULTI_VALUE_ARGS DRIVERS)
     cmake_parse_arguments(PARAM "" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}" ${ARGN})
     execute_process(
-        COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_LIST_DIR}/nxp_sdk_parser.py --sdk_root ${PARAM_SDK_ROOT} ${PARAM_DRIVERS}
+        COMMAND ${Python3_EXECUTABLE} ${_NXP_SDK_PYTHON_SCRIPT_PATH} --sdk_root ${PARAM_SDK_ROOT} ${PARAM_DRIVERS}
         COMMAND_ECHO STDERR
         OUTPUT_VARIABLE STDOUT_RESULT
     )
+    string(STRIP "${STDOUT_RESULT}" STDOUT_RESULT)
     set(${PARAM_VARIABLE} ${STDOUT_RESULT} PARENT_SCOPE)
 endfunction()
 
