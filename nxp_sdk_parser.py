a8790e591fd260ede8405335150f28ad6936cd25
Logiase Song <logiase.song@gmail.com>
2024-01-08 00:48:06 +0800

    Update python script

--- nxp_sdk_parser.py
+++ nxp_sdk_parser.py
@@ -13,6 +13,102 @@ class DriverMeta(object):
         self.srcs = srcs
 
 
+class Core(object):
+    def __init__(self, name: str, fpu: bool, dsp: bool):
+        self.name = name
+        self.fpu = fpu
+        self.dsp = dsp
+
+
+class Device(object):
+    def __init__(
+        self, name: str, cores: List[Core], packages: List[str], defines: List[str]
+    ):
+        self.name = name
+        self.cores = cores
+        self.packages = packages
+        self.defines = defines
+
+    def get_defines(self, package, core) -> List[str]:
+        if package not in self.packages:
+            raise ValueError()
+        cores = [c.name for c in self.cores]
+        if core not in cores:
+            raise ValueError()
+
+        defines = []
+        for d in self.defines:
+            defines.append(
+                d.replace("$|package|", package).replace("$|core_name|", core)
+            )
+        return defines
+
+
+class SDK(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def parse(cls, dir):
+        xmlfile = glob(os.path.join(dir, "a.xml"))
+        if len(xmlfile) != 1:
+            print(f"SDK meta file not found in {dir}", file=sys.stderr)
+            raise FileNotFoundError()
+        xmlfile = xmlfile[0]
+
+        tree = ET.parse(xmlfile)
+        root = tree.getroot()
+
+        # find devices
+        devices = []
+        devices_meta = root.findall("./devices/device")
+        for meta in devices_meta:
+            packages = []
+            defines = []
+            cores = []
+            mems = []
+            for one in meta.findall("./package"):
+                packages.append(one.attrib["name"])
+            for one in meta.findall("./defines/define"):
+                defines.append(one.attrib["name"])
+            for one in meta.findall("./core"):
+                name = one.attrib["name"]
+                fpu = True if one.attrib["fpu"] == "true" else False
+                dsp = True if one.attrib["dsp"] == "true" else False
+                cores.append(Core(name, fpu, dsp))
+            for one in meta.findall("./memory/memoryBlock"):
+                pass
+            devices.append(Device(meta.attrib["name"], cores, packages, defines))
+
+        # find drivers
+        drivers = {}
+        for device in devices:
+            drivers[device.name] = {}
+        comps = root.findall("./components/component")
+        # for comp in comps:
+        #     id = comp.attrib["id"]
+        #     if (not id.startswith("platform.drivers.")) or (
+        #         not id.startswith("platform.devices.")
+        #     ):
+        #         pass
+        for comp in comps:
+            id = comp.attrib["id"]
+            process = False
+
+            if id.startswith("platform.drivers."):
+                process = True
+                id = id.removeprefix("platform.drivers.")
+
+            if id.startswith("platform.devices."):
+                process = True
+                id = id.removesuffix("platform.devices.")
+
+            if not process:
+                continue
+
+        return cls()
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--sdk_root", required=True)
@@ -22,7 +118,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-
+    
     xmlfile = glob(os.path.join(args.sdk_root, "*.xml"))
     if not len(xmlfile) == 1:
         print(f"SDK meta file not found in {args.sdk_root}", file=sys.stderr)
