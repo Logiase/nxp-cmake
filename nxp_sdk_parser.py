c79a39e88955dd8afc99c09eb68b4c97e1ebae75
Logiase Song <logiase.song@nxp.com>
2024-01-08 14:47:53 +0800

    Update script for more functions

--- nxp_sdk_parser.py
+++ nxp_sdk_parser.py
@@ -1,9 +1,12 @@
 import argparse
 import os
 import sys
+import re
 from glob import glob
 from typing import List
 from xml.etree import ElementTree as ET
+
+driver_regex = r"platform\.(drivers|devices)\.(.*?(?=\.))\.(.*)"
 
 
 class DriverMeta(object):
@@ -13,23 +16,73 @@ class DriverMeta(object):
         self.srcs = srcs
 
 
+class Driver(object):
+    def __init__(self, name, dependencies: List[str], srcs: List[str]):
+        self.name = name
+        self.dependencies = dependencies
+        self.srcs = srcs
+
+    @classmethod
+    def parse(cls, node: ET.Element) -> "Driver":
+        name = node.attrib["name"]
+        dependencies = []
+        dependencie_nodes = [
+            *node.findall("./dependencies/component_dependency"),
+            *node.findall("./dependencies/all/component_dependency"),
+        ]
+        for n in dependencie_nodes:
+            matches = re.findall(driver_regex, n.attrib["value"])
+            if len(matches) != 1:
+                continue
+            matches = matches[0]
+            if matches[0] != "drivers":
+                continue
+            dependencies.append(matches[1])
+        srcs = []
+        src_nodes = node.findall("./source[@type='src']")
+        for src_node in src_nodes:
+            for file_node in src_node.findall("./files"):
+                srcs.append(
+                    os.path.join(
+                        node.attrib["package_base_path"],
+                        src_node.attrib["relative_path"],
+                        file_node.attrib["mask"],
+                    )
+                )
+
+        return cls(name, dependencies, srcs)
+
+
 class Core(object):
     def __init__(self, name: str, fpu: bool, dsp: bool):
         self.name = name
         self.fpu = fpu
         self.dsp = dsp
 
+    @classmethod
+    def parse(cls, node: ET.Element):
+        name = node.attrib["name"]
+        fpu = True if node.attrib["fpu"] == "true" else False
+        dsp = True if node.attrib["dsp"] == "true" else False
+        return cls(name, fpu, dsp)
+
 
 class Device(object):
     def __init__(
-        self, name: str, cores: List[Core], packages: List[str], defines: List[str]
+        self,
+        name: str,
+        cores: List[Core],
+        packages: List[str],
+        defines: List[str],
+        drivers: dict[str, Driver],
     ):
         self.name = name
         self.cores = cores
         self.packages = packages
         self.defines = defines
+        self.drivers = drivers
 
-    def get_defines(self, package, core) -> List[str]:
+    def get_defines(self, package: str, core: str) -> List[str]:
         if package not in self.packages:
             raise ValueError()
         cores = [c.name for c in self.cores]
@@ -43,158 +96,141 @@ class Device(object):
             )
         return defines
 
+    def append_driver(self, driver: Driver):
+        self.drivers.append(driver)
+
+    def append_drivers(self, drivers: List[Driver]):
+        self.drivers.extend(drivers)
+
 
 class SDK(object):
-    def __init__(self):
-        pass
+    def __init__(self, devices: dict[str, Device]):
+        self.devices = devices
+
+    def get_device(self, name):
+        if name is None and len(self.devices) == 1:
+            return self.devices[list(self.devices.keys())[0]]
+        if name not in self.devices:
+            raise KeyError(f"device {name} not found")
+        return self.devices[name]
 
     @classmethod
     def parse(cls, dir):
-        xmlfile = glob(os.path.join(dir, "a.xml"))
+        xmlfile = glob(os.path.join(dir, "*.xml"))
         if len(xmlfile) != 1:
-            print(f"SDK meta file not found in {dir}", file=sys.stderr)
-            raise FileNotFoundError()
+            raise FileNotFoundError(f"SDK meta file not found in {dir}")
         xmlfile = xmlfile[0]
 
         tree = ET.parse(xmlfile)
         root = tree.getroot()
 
         # find devices
-        devices = []
-        devices_meta = root.findall("./devices/device")
-        for meta in devices_meta:
+        devices = {}
+        device_nodes = root.findall("./devices/device")
+        for device_node in device_nodes:
+            name = device_node.attrib["name"]
             packages = []
             defines = []
             cores = []
-            mems = []
-            for one in meta.findall("./package"):
-                packages.append(one.attrib["name"])
-            for one in meta.findall("./defines/define"):
-                defines.append(one.attrib["name"])
-            for one in meta.findall("./core"):
-                name = one.attrib["name"]
-                fpu = True if one.attrib["fpu"] == "true" else False
-                dsp = True if one.attrib["dsp"] == "true" else False
-                cores.append(Core(name, fpu, dsp))
-            for one in meta.findall("./memory/memoryBlock"):
-                pass
-            devices.append(Device(meta.attrib["name"], cores, packages, defines))
+            drivers = {}
 
-        # find drivers
-        drivers = {}
-        for device in devices:
-            drivers[device.name] = {}
-        comps = root.findall("./components/component")
-        # for comp in comps:
-        #     id = comp.attrib["id"]
-        #     if (not id.startswith("platform.drivers.")) or (
-        #         not id.startswith("platform.devices.")
-        #     ):
-        #         pass
-        for comp in comps:
-            id = comp.attrib["id"]
-            process = False
+            for package_node in device_node.findall("./package"):
+                packages.append(package_node.attrib["name"])
+            for define_node in device_node.findall("./defines/define"):
+                defines.append(define_node.attrib["name"])
+            for core_node in device_node.findall("./core"):
+                cores.append(Core.parse(core_node))
+            for driver_node in set(
+                root.findall("./components/component[@type='driver']")
+            ) & set(root.findall(f"./components/component[@devices='{name}']")):
+                # drivers.append(Driver.parse(driver_node))
+                drivers[driver_node.attrib["name"]] = Driver.parse(driver_node)
+            devices[name] = Device(name, cores, packages, defines, drivers)
 
-            if id.startswith("platform.drivers."):
-                process = True
-                id = id.removeprefix("platform.drivers.")
-
-            if id.startswith("platform.devices."):
-                process = True
-                id = id.removesuffix("platform.devices.")
-
-            if not process:
-                continue
-
-        return cls()
+        return cls(devices)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--sdk_root", required=True)
-    parser.add_argument("drivers", nargs="+")
-    return parser.parse_args()
-
-
-def main():
-    args = parse_args()
-    
-    xmlfile = glob(os.path.join(args.sdk_root, "*.xml"))
-    if not len(xmlfile) == 1:
-        print(f"SDK meta file not found in {args.sdk_root}", file=sys.stderr)
-        exit(1)
-    xmlfile = xmlfile[0]
-
-    tree = ET.parse(xmlfile)
-    root = tree.getroot()
-    device = root.attrib["name"]
-    map = {}
-
-    comps = root.findall("./components/")
-    for comp in comps:
-        id = comp.attrib["id"]
-        if not id.startswith("platform.drivers."):
-            continue
-        id = id.removeprefix("platform.drivers.").removesuffix("." + device)
-
-        dependencies = []
-        deps = comp.findall("./dependencies/all/component_dependency")
-        for dep in deps:
-            dep_id = dep.attrib["value"]
-            if not dep_id.startswith("platform.drivers."):
-                continue
-            dependencies.append(
-                dep_id.removeprefix("platform.drivers.").removesuffix("." + device)
-            )
-        deps = comp.findall("./dependencies/component_dependency")
-        for dep in deps:
-            dep_id = dep.attrib["value"]
-            if not dep_id.startswith("platform.drivers."):
-                continue
-            dependencies.append(
-                dep_id.removeprefix("platform.drivers.").removesuffix("." + device)
-            )
-
-        sources = []
-        srcs = comp.findall("./source/")
-        for src in srcs:
-            fname = src.attrib["mask"]
-            if not fname.endswith(".c"):
-                continue
-            sources.append(fname)
-
-        map[id] = DriverMeta(id, dependencies, sources)
+def get_sources(args):
+    sdk = SDK.parse(args.sdk_root)
+    if args.device is None:
+        if len(sdk.devices) != 1:
+            raise ValueError("SDK contains more than 1 devices, please specific one")
+        device = sdk.get_device(None)
+    else:
+        device = sdk.get_device(args.device)
 
     required = args.drivers
     required = set(required)
     fin = set()
 
     for c in required:
-        if not c in map:
-            print(f"{c} not found, ignore it", file=sys.stderr)
-            continue
+        if not c in device.drivers:
+            raise KeyError(f"driver {c} not found")
         fin.add(c)
-
     while True:
         last_len = len(fin)
         required.clear()
         for c in fin:
-            if not c in map:
-                print(f"{c} not found, ignore it", file=sys.stderr)
-                continue
-            for dep in map[c].dependencies:
+            if not c in device.drivers:
+                raise KeyError(f"driver {c} not found")
+            for dep in device.drivers[c].dependencies:
                 required.add(dep)
         fin = fin.union(required)
         if len(fin) == last_len:
             break
 
     srcs = set()
-    driver_path = os.path.join(args.sdk_root, f"devices/{device}/drivers")
     for c in fin:
-        d = map[c]
-        for f in d.srcs:
-            srcs.add(os.path.abspath(os.path.join(driver_path, f)))
+        driver = device.drivers[c]
+        for f in driver.srcs:
+            srcs.add(os.path.abspath(os.path.join(args.sdk_root, f)))
     print(*srcs, sep=";")
+
+
+def get_defines(args):
+    print("defines")
+
+
+def get_includes(args):
+    print("includes")
+
+
+def print_list(args):
+    print("list")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--sdk_root", required=True)
+    subparsers = parser.add_subparsers(title="subcommands")
+
+    parser_sources = subparsers.add_parser("sources")
+    parser_sources.add_argument("--device")
+    parser_sources.add_argument("drivers", nargs="+")
+    parser_sources.set_defaults(func=get_sources)
+
+    parser_defines = subparsers.add_parser("defines")
+    parser_defines.add_argument("--device")
+    parser_defines.set_defaults(func=get_defines)
+
+    parser_includes = subparsers.add_parser("includes")
+    parser_includes.add_argument("--device")
+    parser_includes.add_argument("--cmsis", action="store_true")
+    parser_includes.set_defaults(func=get_includes)
+
+    parser_list_deviec = subparsers.add_parser("list_device")
+    parser_list_deviec.set_defaults(func=print_list)
+    parser_list_drivers = subparsers.add_parser("list_drivers")
+    parser_list_drivers.set_defaults(func=print_list)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    args.func(args)
+    return
 
 
 if __name__ == "__main__":
